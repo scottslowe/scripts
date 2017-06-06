c1f0cc472c678a51447de773b3acaa47dc086afb
Scott Lowe <scott.lowe@scottlowe.org>
2017-06-06 09:31:34 -0600

    Switch from if-then to case-esac
    Use a single `case..esac` structure instead of multiple `if..then` statements
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

--- distro-test.sh
+++ distro-test.sh
@@ -3,17 +3,32 @@
 # Gather kernel version for evaluation
 REL=$(uname -rv)
 
-# Test for Ubuntu
-if [[ $REL == *"Ubuntu"* ]]; then
-    echo "Ubuntu"
-fi
+# Test to determine OS/release/distro
+case $REL in
+    *Ubuntu*)
+        echo "Ubuntu"
+        ;;
+    *Darwin*)
+        echo "macOS"
+        ;;
+    *fc*)
+        echo "Fedora"
+        ;;
+    *)
+        echo "Unknown"
+        ;;
+esac
 
+#if [[ $REL == *"Ubuntu"* ]]; then
+#    echo "Ubuntu"
+#fi
+#
 # Test for macOS
-if [[ $REL == *"Darwin"* ]]; then
-    echo "macOS"
-fi
-
+#if [[ $REL == *"Darwin"* ]]; then
+#    echo "macOS"
+#fi
+#
 # Test for Fedora
-if [[ $REL == *"fc"* ]]; then
-    echo "Fedora"
-fi
+#if [[ $REL == *"fc"* ]]; then
+#    echo "Fedora"
+#fi
